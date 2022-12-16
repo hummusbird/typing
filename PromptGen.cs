66ca3d31ec9ad253ebedbe8e012935265cb9f45e
hummusbird <birb@birb.cc>
2022-12-16 15:21:57 +0000

    promptgen init

---
+++ PromptGen.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace typing
+{
+    static class PromptGen
+    {
+        public static string GenerateRandomFromDictionary(int words)
+        {
+            return "g";
+        }
+    }
+}
