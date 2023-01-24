1ae24b374da6c64b60fe9c038b087ee1ec6209ff
hummusbird <birb@birb.cc>
2023-01-24 17:21:41 +0000

    arg handling

--- Program.cs
+++ Program.cs
@@ -4,15 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        // TODO: handle args
-        // TODO: interactive settings menu
-        // TODO: default settings .conf file
-        // TODO: prompt gen
-
         string prompt = PromptGen.GenerateRandomPromptFromWordlist(10); // "the quick brown fox jumped over the lazy dog, before going on a shopping trip to Tesco's. The dog bought some cheese.";
         int timer = 10;
         bool ghost = true;
         bool autocorrect = false;
+
+        foreach (string arg in args)
+        {
+            int pos = Array.FindIndex(args, v => v == arg);
+
+            switch (arg)
+            {
+                case "-t": // timer length
+                    if (args.Length >= pos + 2)
+                    {
+                        timer = int.Parse(args[pos + 1]);
+                    }
+                    break;
+            }
+        }
+
+
+        // TODO: interactive settings menu
+        // TODO: default settings .conf file
+        // TODO: prompt gen
 
         TypingTest newtest = new(@prompt, timer, ghost, autocorrect);
 
